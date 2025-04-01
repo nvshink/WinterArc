b9774959a631ed169b6f1390522544831c9c26ae
nvshink <62372855+nvshink@users.noreply.github.com>
2025-04-01 17:22:10 +0600

    Datasource fix

--- app/src/main/java/com/example/winterarc/data/Datasource.kt
+++ app/src/main/java/com/example/winterarc/data/Datasource.kt
@@ -2,18 +2,13 @@ package com.example.winterarc.data
 
 import com.example.winterarc.data.model.Exercise
 import com.example.winterarc.data.model.TrainingPlan
+import com.example.winterarc.data.model.TrainingPlanExercise
 import com.example.winterarc.data.model.User
 
-//import com.example.affirmations.R
-//import com.example.affirmations.model.Affirmation
-
-/**
- * [Datasource] generates a list of [Affirmation]
- */
 object Datasource {
-    val loremIpsum: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis, felis sed rutrum pretium, arcu metus aliquet justo, ac bibendum elit felis eget nisl. Aenean nec sem vitae lorem condimentum euismod ut nec ante. In vitae felis scelerisque, varius nunc nec, venenatis felis. Suspendisse augue nisi, iaculis vel lorem sit amet, porta ullamcorper diam. Etiam fringilla ac arcu nec iaculis. Donec scelerisque in est in cursus. Mauris volutpat eros ac mi pretium, non convallis ante placerat. In massa nunc, elementum dapibus ultrices a, condimentum et tortor. Nulla tempor convallis turpis ut sagittis."
+    private const val loremIpsum: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis, felis sed rutrum pretium, arcu metus aliquet justo, ac bibendum elit felis eget nisl. Aenean nec sem vitae lorem condimentum euismod ut nec ante. In vitae felis scelerisque, varius nunc nec, venenatis felis. Suspendisse augue nisi, iaculis vel lorem sit amet, porta ullamcorper diam. Etiam fringilla ac arcu nec iaculis. Donec scelerisque in est in cursus. Mauris volutpat eros ac mi pretium, non convallis ante placerat. In massa nunc, elementum dapibus ultrices a, condimentum et tortor. Nulla tempor convallis turpis ut sagittis."
     fun loadExercises(): List<Exercise> {
-        return listOf<Exercise>(
+        return listOf(
             Exercise(1, "Ажумания", listOf(""), "жум жум"),
             Exercise(2,"Приседат", listOf(""), "жум жум"),
             Exercise(3,"Жым", listOf(""), "жум жум"),
@@ -29,43 +24,70 @@ object Datasource {
 
 
     private val backListExercises: List<Exercise> = loadExercises().subList(0, 4)
-    private val backListExercisesTrainingPlan: List<Triple<Exercise, Int, Boolean>> = listOf(
-        Triple(
-            backListExercises[0], 10, true
+    private val backListExercisesTrainingPlan: List<TrainingPlanExercise> = listOf(
+        TrainingPlanExercise(
+            id = 0,
+            exercise = backListExercises[0],
+            duration = 10,
+            isInSets = true
         ),
-        Triple(
-            backListExercises[1], 20, true
+        TrainingPlanExercise(
+            id = 1,
+            exercise = backListExercises[1],
+            duration = 20,
+            isInSets = true
         ),
-        Triple(
-            backListExercises[2], 10, true
+        TrainingPlanExercise(
+            id = 2,
+            exercise = backListExercises[2],
+            duration = 10,
+            isInSets = true
         ),
-        Triple(
-            backListExercises[3], 5, true
+        TrainingPlanExercise(
+            id = 3,
+            exercise = backListExercises[3],
+            duration = 15,
+            isInSets = true
         )
     )
     private val legListExercises: List<Exercise> = loadExercises().subList(4, 6)
-    private val legListExercisesTrainingPlan: List<Triple<Exercise, Int, Boolean>> = listOf(
-        Triple(
-            legListExercises[0], 300, false
+    private val legListExercisesTrainingPlan: List<TrainingPlanExercise> = listOf(
+        TrainingPlanExercise(
+            id = 0,
+            exercise = legListExercises[0],
+            duration = 300,
+            isInSets = false
         ),
-        Triple(
-            legListExercises[1], 20, true
+        TrainingPlanExercise(
+            id = 1,
+            exercise = legListExercises[1],
+            duration = 20,
+            isInSets = true
         )
     )
     private val armListExercises: List<Exercise> = loadExercises().subList(6, 9)
-    private val armListExercisesTrainingPlan: List<Triple<Exercise, Int, Boolean>> = listOf(
-        Triple(
-            armListExercises[0], 10, true
+    private val armListExercisesTrainingPlan: List<TrainingPlanExercise> = listOf(
+        TrainingPlanExercise(
+            id = 0,
+            exercise = armListExercises[0],
+            duration = 10,
+            isInSets = true
         ),
-        Triple(
-            armListExercises[1], 20, true
+        TrainingPlanExercise(
+            id = 1,
+            exercise = armListExercises[1],
+            duration = 20,
+            isInSets = true
         ),
-        Triple(
-            armListExercises[2], 100, false
+        TrainingPlanExercise(
+            id = 2,
+            exercise = armListExercises[0],
+            duration = 100,
+            isInSets = false
         )
     )
     fun loadTrainingPlan(): List<TrainingPlan> {
-        return listOf<TrainingPlan>(
+        return listOf(
             TrainingPlan(1,"Спина", loremIpsum, backListExercisesTrainingPlan),
             TrainingPlan(2, "Ноги", loremIpsum, legListExercisesTrainingPlan),
             TrainingPlan(3, "Руки", loremIpsum, armListExercisesTrainingPlan)
